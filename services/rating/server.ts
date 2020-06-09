abaede087e6e1c93bb219d94e047fd5dcc8e2190
Guilherme Camargo <guisfits@hotmail.com>
2020-06-08 21:15:20 -0300

    configuration

--- services/rating/server.ts
+++ services/rating/server.ts
@@ -1,9 +1,7 @@
 import { Application } from 'https://deno.land/x/oak/mod.ts'
-import router from './routes.ts'
 
-const env = Deno.env.toObject();
-const HOST = env.HOST || '127.0.0.1';
-const PORT = env.PORT || 8001;
+import { HOST, PORT } from './config.ts';
+import router from './routes.ts'
 
 const app = new Application();
 
