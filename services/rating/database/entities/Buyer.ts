d9a9cb70e1378c45f68fa965e0bb9f793893cf28
Guilherme Camargo <guisfits@hotmail.com>
2020-06-08 22:44:24 -0300

    entities

---
+++ services/rating/database/entities/Buyer.ts
@@ -0,0 +1,4 @@
+export default interface Buyer {
+  id: string;
+  name: string;
+}
---
+++ services/rating/database/entities/Rating.ts
@@ -0,0 +1,6 @@
+export default interface Rating {
+  id: string;
+  productId: string;
+  rate: number;
+  comment: string;
+}
