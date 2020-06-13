3b0e3fc39839fb0e43169c27a6da8f206486c8c1
Guilherme Camargo <guisfits@hotmail.com>
2020-06-13 16:49:45 -0300

    select ratings

--- services/rating/controllers/rating.controller.ts
+++ services/rating/controllers/rating.controller.ts
@@ -4,11 +4,24 @@ class RatingController {
 
   async index({ response }: { response: any }) {
 
-    const ratings = await repository.instance.find();
+    const ratings = await repository.instance
+      .createQueryBuilder('ratings')
+      .select(['ratings', 'product.name', 'buyer.firstName'])
+      .leftJoin('ratings.product', 'product')
+      .leftJoin('ratings.buyer', 'buyer')
+      .getMany();
 
-    response.body = { 
-      success: true, 
-      data: ratings 
+    response.body = {
+      success: true,
+      data: ratings.map(rating => {
+        return {
+          id: rating.id,
+          comment: rating.comment,
+          rate: rating.rate,
+          buyer: rating.buyer.firstName,
+          product: rating.product.name
+        }
+      })
     };
   }
 }
