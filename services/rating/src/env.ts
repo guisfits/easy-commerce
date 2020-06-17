8d4239c7f7128a4ff9c5cf7fa0d994d16b6e8f27
Guilherme Camargo <guisfits@hotmail.com>
2020-06-16 21:14:42 -0300

    fixing app_port

--- services/rating/src/env.ts
+++ services/rating/src/env.ts
@@ -1,6 +1,6 @@
 const env = Deno.env.toObject();
 
-export const APP_PORT = Number(env.PORT);
+export const APP_PORT = Number(env.APP_PORT);
 export const DB_HOST = env.DB_HOST;
 export const DB_PORT = Number(env.DB_PORT);
 export const DB_USERNAME = env.DB_USERNAME;
