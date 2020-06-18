b799d2f6378bcfed852eeda18ee730a61df66933
Guilherme Camargo <guisfits@hotmail.com>
2020-06-18 17:37:48 -0300

    seed

--- services/rating/sql/seed.sql
+++ services/rating/sql/seed.sql
@@ -1,37 +1,30 @@
-BEGIN;
+DO $$
+DECLARE 
+	productId integer;
+	buyerId integer;
 
-INSERT INTO
-  buyers("fullName")
-VALUES
-  ('Brunce Wayne');
+BEGIN
+	
+	INSERT INTO buyers("fullName")
+	VALUES ('Bruce Wayne');
+	
+	INSERT INTO products("name")
+	VALUES ('PS4');
+	
+	SELECT id INTO buyerId
+	FROM buyers 
+	WHERE "fullName" = 'Brunce Wayne';
+	
+	SELECT id INTO productId
+	FROM products 
+	WHERE "name" = 'PS4';
+	
+	INSERT INTO
+	  ratings("rate", "comment", "productId", "buyerId")
+	VALUES
+	  (4, 'best console', productId, buyerId),
+	  (2, 'xbox one is better', productId, buyerId);
+ 
+END;
+$$;
 
-INSERT INTO
-  products("name")
-VALUES
-  ('PS4');
-
-productId integer := (
-  select
-    id
-  from
-    products
-  where
-    "name" = 'PS4'
-);
-
-buyerId integer := (
-  select
-    id
-  from
-    buyers
-  where
-    "fullName" = 'Brunce Wayne'
-);
-
-INSERT INTO
-  ratings("rate", "comment", "productId", "buyerId")
-VALUES
-  (4, 'best console', productId, buyerId),
-  (2, 'xbox one is better', productId, buyerId);
-
-COMMIT;
